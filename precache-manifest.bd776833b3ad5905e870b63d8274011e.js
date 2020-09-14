5f4097520efe39cca6e2a0f794429d2f9f5f38c0
semgoSE <semgoSE@gmail.com>
2020-09-14 12:01:29 +0300

    Updates

---
+++ precache-manifest.bd776833b3ad5905e870b63d8274011e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdca0bdcc1265ec42129c98b5d80c6b2",
+    "url": "/sbor_SMEh/index.html"
+  },
+  {
+    "revision": "b295db8fa25b06da622f",
+    "url": "/sbor_SMEh/static/css/2.09e62520.chunk.css"
+  },
+  {
+    "revision": "1b6f9d9919391c595d94",
+    "url": "/sbor_SMEh/static/css/main.572c94ce.chunk.css"
+  },
+  {
+    "revision": "b295db8fa25b06da622f",
+    "url": "/sbor_SMEh/static/js/2.de2265ee.chunk.js"
+  },
+  {
+    "revision": "9545d2ff040f1d230f20c359158fcfad",
+    "url": "/sbor_SMEh/static/js/2.de2265ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b6f9d9919391c595d94",
+    "url": "/sbor_SMEh/static/js/main.38838781.chunk.js"
+  },
+  {
+    "revision": "44055f5bc994a3a5c9b1",
+    "url": "/sbor_SMEh/static/js/runtime-main.d87f7b41.js"
+  }
+]);
